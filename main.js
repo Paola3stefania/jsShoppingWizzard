c74c1c8aadbea39065b71703a20b26197a6cbeba
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-07-06 00:44:05 +0200

    add js

--- main.js
+++
@@ -1,8 +0,0 @@
-var tit = document.getElementsByTagName("h1");
-var tit3 = document.getElementsByTagName("h3")
-tit.appendChild("body")
-tit.addEventListener("click", titchange);
-
-titchange() {
-    document.tit.style.fontSize = "40px"
-}
