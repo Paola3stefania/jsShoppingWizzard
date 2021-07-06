f4cc4793d6c2e2034c2467e1c78d1cceeaedf6a0
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-07-06 14:57:41 +0200

    fix on navigation router

--- js/main.js
+++ js/main.js
@@ -1,26 +1,54 @@
-let wrapper = ''
-let url = location.hash
+// nuestra sección de router
 
-if(url === '#product' || url === '') {
-    product()
-} else if (url === '#buy') {
-  buy()
+window.addEventListener("hashchange", navigate);
+var wrapper = document.getElementById("app");
+
+function navigate() {
+	if (location.hash === "#product" || location.hash === "") {
+		product();
+	} else if (location.hash === "#buy") {
+		buy();
+	}
 }
 
-function product () {
-  wrapper = document.getElementById('app')
-  let product = document.getElementById('product').content
-  let copyProduct = document.importNode(product, true)
-  wrapper.appendChild(copyProduct)
-  
-  document.querySelector('.product__button').addEventListener('click', buy)
+navigate();
+
+//! Functions
+
+function product() {
+	let product = document.getElementById("product").content;
+	let copyProduct = document.importNode(product, true);
+
+	wrapper.appendChild(copyProduct);
+	document.querySelector(".product__button").addEventListener("click", goToBuy);
+}
+
+// new route
+function goToBuy(event) {
+	event.preventDefault();
+	window.location.hash = "#buy";
+}
+
+function goToHomepage() {
+	window.location.hash = "";
 }
 
 function buy() {
-  wrapper = document.getElementById('app')
-  wrapper.removeChild(wrapper.firstChild)
-  let buy = document.getElementById('form').content
-  let copyBuy = document.importNode(buy, true)
-  wrapper.appendChild(copyBuy)
-  console.log('hiiiieee');
+	let nodoToRemove = wrapper.firstElementChild;
+	if (nodoToRemove) {
+		//remove event Listener before deleting de node
+		document
+			.querySelector(".product__button")
+			.removeEventListener("click", goToBuy);
+
+		wrapper.removeChild(nodoToRemove);
+		contador = 1;
+
+		let buy = document.getElementById("form").content;
+		let copyBuy = document.importNode(buy, true);
+
+		wrapper.appendChild(copyBuy);
+	} else {
+		goToHomepage();
+	}
 }
