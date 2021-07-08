b1168e6ecd23bd68f99bb175d82be0799da96dd8
SalvaBandicoot <salvawebdg@gmail.com>
2021-07-08 10:45:23 +0200

    finish fixed

--- js/templateFinish.js
+++ js/templateFinish.js
@@ -4,32 +4,65 @@ import { goToHomepage, goToThankYou } from "./router.js";
 let finishHtml = `
 <template id="finish">
 	<div>
-		<h1>Your purchase</h1>
-		<div>
-			<img>
+		<div id="shopping-bar" class="shopping-bar">
+			<div class="line"></div>
+			<ul class="text-progress">
+				<li><strong>Profile</strong></li>
+				<li><strong>Address</strong></li>
+				<li><strong>Shipping</strong></li>
+				<li><strong>Finish</strong></li>
+			</ul>
+			<ul class="checkpoints">
+				<li>
+					<div class="circle"></div>
+				</li>
+				<li>
+					<div class="circle"></div>
+				</li>
+				<li>
+					<div class="circle"></div>
+				</li>
+				<li>
+					<div class="circle"></div>
+				</li>
+			</ul>
 		</div>
-		<div>
-			<h2>Brown t-shirt</h2>
-			<p>Size: <strong>M</strong></p>
-			<p>Color: </p>
-			<p>Estimate delivery date:</p>
-			<p>Between</p>
-			<p>and</p>
-		</div>
-		<form action="">
-			<imput type="checkbox" id="terms" name="terms" value="Terms and conditions">
-			<label for="terms">I have read and I accept the terms and conditions.</label>
-			<input type="submit" value="Buy Now">
-		</form>
 	</div>
+    <main id="main-content" class="main-content">
+        <div id="form-content" class="form-content">
+			<div>
+				<h1>Your purchase</h1>
+				<div>
+					<img>
+				</div>
+				<div>
+					<h2>Brown t-shirt</h2>
+					<p>Size: <strong>M</strong></p>
+					<p>Color: </p>
+					<p>Estimate delivery date:</p>
+					<p>Between</p>
+					<p>and</p>
+				</div>
+				<form action="">
+					<imput type="checkbox" id="terms" name="terms" value="Terms and conditions">
+					<label for="terms">I have read and I accept the terms and conditions.</label>
+					<input type="submit" value="Buy Now" class="buttons main-button>
+				</form>
+			</div>
+		</div>
+    </main>
 	<div>
 		<p>Your order</p>
 		<hr>
-		<p id="product"></p>
-		<p id="shipping"></p>
+		<p id="product">product</p>
+		<p id="shipping">shipping</p>
 		<hr>
-		<p id="total"></p>
+		<p id="total">total</p>
 	</div>
+	<div id="timer" class="timer"></div>
+
+
+
 </template>
 `;
 
